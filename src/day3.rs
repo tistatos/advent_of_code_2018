810fc48156db86781867886f48c870cdfdb2ef8d
tistatos <tistatos@gmail.com>
2018-12-19 00:16:11 +0100

    day 3 done

--- src/day3.rs
+++ src/day3.rs
@@ -1,6 +1,5 @@
 mod aoc;
 
-use std::collections::HashMap;
 use self::aoc::get_string_rows;
 
 const UNCLAIMED: char = '.';
@@ -29,26 +28,20 @@ impl Claim {
 fn main() -> std::io::Result<()> {
 
     let claims_values = get_string_rows("input_data/day_3");
+    let mut claims = Vec::new();
     let mut fabric = vec![
         vec![UNCLAIMED;1000]; 1000];
 
     let mut total_overlap = 0;
-    let mut non_overlapping_id = 0;
     for claim_values in claims_values {
          let inputs: Vec<u32> =
-            claim_values.split(|c|
-                c == '#' ||
-                c == ' ' ||
-                c == '@' ||
-                c == ',' ||
-                c == ':' ||
-                c == 'x' ).filter(|x| !x.is_empty())
+            claim_values.split(|c| "# @,:x".contains(c))
+            .filter(|x| !x.is_empty())
             .map(|x|
                  x.parse().expect(x)).collect();
-         let claim = Claim {
+         let mut claim = Claim {
                  id: inputs[0], x: inputs[1], y: inputs[2],
                  width: inputs[3], height: inputs[4], overlap: false };
-        let mut overlapped = false;
         for x in 0..claim.width {
             for y in 0..claim.height {
                 let x_index = (claim.x + x) as usize;
@@ -59,24 +52,35 @@ fn main() -> std::io::Result<()> {
                 }
                 else if fabric[x_index][y_index] == CLAIMED {
                     fabric[x_index][y_index] = OVERLAP;
-                    overlapped = true;
+                    claim.overlap = true;
                     total_overlap += 1;
-
+                }
+                else if fabric[x_index][y_index] == OVERLAP {
+                    claim.overlap = true;
                 }
             }
         }
-        if !overlapped {
-            non_overlapping_id = claim.id;
+        if !claim.overlap {
+            claims.push(claim);
         }
     }
-    println!("Day 3 part one result: {}", total_overlap);
-    println!("Day 3 part two result: #{}", non_overlapping_id);
-    //for line in fabric.iter() {
-        //for dot in line.iter() {
-            //print!("{}", dot)
-        //}
-        //println!("");
-    //}
 
+    for c in claims.iter_mut() {
+        for x in c.x..c.end_x() {
+            for y in c.y..c.end_y() {
+                if fabric[x as usize][y as usize] == 'X' {
+                    c.overlap = true;
+                    break;
+                }
+            }
+            if c.overlap {
+                break
+            }
+        }
+    }
+    claims.retain(|c| !c.overlap);
+
+    println!("Day 3 part one result: {}", total_overlap);
+    println!("Day 3 part two result: #{}", claims[0].id);
     Ok(())
 }
